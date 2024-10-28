02d14e39cc09942d8258eddf1009c463a58be204
jinnieusLab <kitty14904@naver.com>
2024-10-27 19:50:14 -0500

    :sparkles: [feat] 컨테이너 내 애플리케이션 빈 조회

--- src/main/java/hello/core/beanfind/ApplicationContextInfoTest.java
+++ src/main/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContextInfoTest {
@@ -19,4 +20,18 @@ public class ApplicationContextInfoTest {
         }
     }
 
+    @Test
+    @DisplayName("애플리케이션 빈 출력하기")
+    void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + "\nobject = " + bean + "\n");
+            }
+        }
+    }
 }
