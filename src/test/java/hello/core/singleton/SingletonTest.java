c9432c0f73c503f92eeacc72b9afe535fca4a5c9
jinnieusLab <kitty14904@naver.com>
2025-07-01 21:20:52 +0900

    :sparkles: [feat] 스프링 없이 순수한 DI 컨테이너 구현 (싱글톤 x)

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,26 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링이 없는 순수한 DI 컨테이너")
+    void pureContainer () {
+        AppConfig appConfig = new AppConfig();
+        // 조회할 때마다 멤버 서비스 생성
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 서로 다름
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
