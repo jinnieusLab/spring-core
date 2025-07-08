eff4cf689c37aa46859ae406ad027310955dd630
jinnieusLab <kitty14904@naver.com>
2025-07-09 00:35:14 +0900

    :sparkles: [fix] 싱글톤 - stateless 수정

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,11 +2,11 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price;
+//    private int price;
 
     public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price;
+//        this.price = price;
         return price;
     }
 
