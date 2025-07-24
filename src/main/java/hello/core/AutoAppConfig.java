74c90b0d0a50c60e93f01d8c39389ec817114a82
jinnieusLab <kitty14904@naver.com>
2025-07-24 14:49:25 +0900

    :sparkles: [feat] Component Scan 시작 위치 지정

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,5 +10,6 @@ import org.springframework.context.annotation.FilterType;
         basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
+
 public class AutoAppConfig {
 }
