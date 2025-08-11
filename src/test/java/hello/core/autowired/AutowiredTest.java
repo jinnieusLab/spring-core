d3a552f53b2777fcd6e24a3f8bfc9c86129eb1eb
jinnieusLab <kitty14904@naver.com>
2025-08-11 22:37:57 +0900

    :sparkles: [feat] @Autowired 옵션 처리

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -11,26 +11,26 @@ import java.util.Optional;
 
 public class AutowiredTest {
 
-//    @Test
-//    void AutowiredOption() {
-//        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
-//    }
+    @Test
+    void AutowiredOption() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
 
-//    static class TestBean {
-//
-//        @Autowired(required = false)
-//        public void setNoBean1(Member noBean1) {
-//            System.out.println("noBean1 = " + noBean1);
-//        }
-//
-//        @Autowired
-//        public void setNoBean2(@Nullable Member noBean2) {
-//            System.out.println("noBean2 = " + noBean2);
-//        }
-//
-//        @Autowired(required = false)
-//        public void setNoBean3(Optional<Member> noBean3) {
-//            System.out.println("noBean3 = " + noBean3);
-//        }
-//    }
+    static class TestBean {
+
+        @Autowired(required = false)
+        public void setNoBean1(Member noBean1) {
+            System.out.println("noBean1 = " + noBean1);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member noBean2) {
+            System.out.println("noBean2 = " + noBean2);
+        }
+
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> noBean3) {
+            System.out.println("noBean3 = " + noBean3);
+        }
+    }
 }
