f08e98fc327b5f28d13352d6c4ac43ab2832ce52
jinnieusLab <kitty14904@naver.com>
2025-08-11 22:37:25 +0900

    :bug: [fix] 생성자 주입으로 수정

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,41 +9,43 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    // 생성자 주입 시 필요
-//    private final MemberRepository memberRepository;
-//    private final DiscountPolicy discountPolicy;
+//     생성자 주입 시 필요
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-//    // 수정자 주입
-//    @Autowired(required = false)
-//    public void setMemberRepository (MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy (DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
-
-//    // 생성자 주입
-//    @Autowired
+    // 생성자 주입
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-    // 일반 메서드 주입 시 필요
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
+//    // 수정자 주입 시 필요
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    // 수정자 주입
+//    @Autowired(required = false)
+//    public void setMemberRepository (MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy (DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
 
-    // 일반 메서드 주입
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // 일반 메서드 주입 시 필요
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+
+//    // 일반 메서드 주입
+//    @Autowired
+//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
